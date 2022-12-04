3bc3cd0690c22608bf4af7936b98772518b038ae
Anegm10 <ahmednegm803@gmail.com>
2022-12-05 00:29:31 +0200

    adding online wallet class

---
+++ src/OnlineWallet.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class OnlineWallet implements Payment{
+	Scanner myObj = new Scanner(System.in); 
+	String email;	
+	String UserName;
+	String Password;
+	
+	public void getInfo() {
+		System.out.println("Enter your name...  ");
+	    UserName = myObj.nextLine();
+	   
+		System.out.println("Enter your email name... ");
+		email = myObj.nextLine();
+		
+		System.out.println("Enter your password ... ");
+		Password = myObj.nextLine();
+		
+	}
+
+
+	@Override
+	public void pay(int total) {
+		if(email != null&&UserName!=null&&Password!=null)
+			System.out.println(total +" pay with online wallet");
+		else
+			System.out.println(" Enter a inputs for your wallet please...");
+	}
+}
