a372ccef5a9cf1b4cad72597ce4c245211dfee8f
Anegm10 <ahmednegm803@gmail.com>
2022-12-05 00:51:16 +0200

    update cash pay class

--- src/CashPay.java
+++ src/CashPay.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
-public class CashPay implements Payment{
-	Scanner myObj = new Scanner(System.in); 
+public class CashPay implements Payment {
+	Scanner myObj = new Scanner(System.in);
 
 	String UserName;
+
 	public void getInfo() {
 		System.out.println("Enter your name...  ");
-	    UserName = myObj.nextLine();
-	   
+		UserName = myObj.nextLine();
+
 	}
-	
+
 	@Override
-	public void pay(int total) {
-		if(UserName != null)
-			System.out.println(total +" Cash on pay ");
+	public void pay() {
+		if (UserName != null)
+			System.out.println("correct user info Cash on pay ");
 		else
-			System.out.println(" Enter a correct cridit card number please...");	
+			System.out.println(" Enter a correct cridit card number please...");
 	}
 }
