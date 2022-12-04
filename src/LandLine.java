0e20e7754db8c80735bfe9a07ffbdf0dd43f7ff6
Safeer <ranaosama.h1@gmail.com>
2022-12-04 23:46:10 +0200

    adding landline class

---
+++ src/LandLine.java
@@ -0,0 +1,14 @@
+public class LandLine extends Services{
+	String ServicesName="LandLine";
+
+	public void Display() {
+		System.out.println("Its Land Line Services lets pay now...");
+		
+	}
+
+	//@Override
+	//protected Services AddServices(String name) {
+		//System.out.println("services "+ name+" add ...");
+		//return null;
+	//}
+}
