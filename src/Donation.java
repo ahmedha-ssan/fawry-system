add921163d576dd0ed5bb849c4ce524d4460278f
Anegm10 <ahmednegm803@gmail.com>
2022-12-04 18:27:35 +0200

    adding donation class

---
+++ src/Donation.java
@@ -0,0 +1,9 @@
+public class Donation extends Services {
+	String ServicesName = "Donation";
+
+	public void Display() {
+		System.out.println("Its Dontaion Services lets help others now...");
+
+	}
+
+}
