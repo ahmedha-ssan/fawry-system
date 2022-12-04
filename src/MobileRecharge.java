7222b9f2b41d998e270c2a1cf70257eb3b6b5d30
Anegm10 <ahmednegm803@gmail.com>
2022-12-04 17:56:44 +0200

    adding mobile recharge class

---
+++ src/MobileRecharge.java
@@ -0,0 +1,10 @@
+public class MobileRecharge extends Services{
+	String ServicesName="MobileRecharge";
+	
+	public void Display() {
+		System.out.println("Its Mobile Recharge Services lets recharge now...");	
+	}
+	
+	
+	
+}
