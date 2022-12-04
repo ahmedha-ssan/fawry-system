522a104ebdb7401cfb7b450638cec978c1d4511c
Anegm10 <ahmednegm803@gmail.com>
2022-12-05 00:49:36 +0200

    online wallet class

--- src/OnlineWallet.java
+++ src/OnlineWallet.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
-public class OnlineWallet implements Payment{
-	Scanner myObj = new Scanner(System.in); 
-	String email;	
+public class OnlineWallet implements Payment {
+	Scanner myObj = new Scanner(System.in);
+	String email;
 	String UserName;
 	String Password;
-	
+
 	public void getInfo() {
 		System.out.println("Enter your name...  ");
-	    UserName = myObj.nextLine();
-	   
+		UserName = myObj.nextLine();
+
 		System.out.println("Enter your email name... ");
 		email = myObj.nextLine();
-		
+
 		System.out.println("Enter your password ... ");
 		Password = myObj.nextLine();
-		
+
 	}
 
-
 	@Override
-	public void pay(int total) {
-		if(email != null&&UserName!=null&&Password!=null)
-			System.out.println(total +" pay with online wallet");
+	public void pay() {
+		if (email != null && UserName != null && Password != null)
+			System.out.println("correct info for pay with online wallet");
 		else
 			System.out.println(" Enter a inputs for your wallet please...");
 	}
