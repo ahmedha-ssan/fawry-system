f1608d20dfe12e7b33762282a68cfd479646dbd7
ahmedha-ssan <119657406+ahmedha-ssan@users.noreply.github.com>
2022-12-05 19:20:48 +0200

    add test on main

--- src/Mainn.java
+++ src/Mainn.java
@@ -1,6 +1,47 @@
 public class Mainn {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome on our software system");
+		System.out.println();
+		// object from account
+		Account a = new Account();
+
+		System.out.println("User sign in and admin check if his info is exist or not...");
+		System.out.println();
+		
+		a.SignIn("Ahmed@gmail.com", "123456");
+		System.out.println("Is exist or not : " + a.Check("Ahmed@gmail.com", "123456"));
+		System.out.println();
+
+		a.SignIn("Ahmed@gmail.com", "3333");
+		System.out.println("Is exist or not : " + a.Check("Ahmed@gmail.com", "3333"));
+		System.out.println();
+
+		a.SignUp("saad", "Saad@gmail.com", "111");
+
+		System.out.println();
+		System.out.println("/////////////////////////////////////////////////////////////////");
+		System.out.println();
+
+		// obj from services class to access its sub class and methods
+		// Services service = new Services();
+		// service.getServices("MobileRecharge");
+		System.out.println("Admin accept or reject the refund request...");
+		System.out.println();
+		Refund refund1 = new AcceptRefund();
+		Refund refund2 = new RejectRefund();
+
+		refund1.state();
+		refund2.state();
+		System.out.println();
+
+		System.out.println("Admin list the refund requests...");
+		System.out.println();
+
+		System.out.println(refund1.listReq("MobileRecharge", "50"));
+		System.out.println(refund1.listReq("Donation", "500"));
+
+		System.out.println();
 		System.out.println("/////////////////////////////////////////////////////////////////");
 		System.out.println();
 
